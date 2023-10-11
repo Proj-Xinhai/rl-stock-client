28a370c4c74e32e3495712b24dcfab94c0421e2e
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2023-10-11 21:16:11 +0800

    fix redirect

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// force http
-if (location.protocol !== 'http:') location.replace(`http:${location.href.substring(location.protocol.length)}`)
-
 const app = createApp(App)
 
 app.use(router)
