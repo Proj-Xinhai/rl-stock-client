d53e0424bb04e0545c457b71b97f91779ab10ee5
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2023-10-18 13:13:33 +0800

    component of tasks list

---
+++ src/components/TheTasksList.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import TheTaskRemover from "@/components/TheTaskRemover.vue"
+import TheTaskExporter from "@/components/TheTaskExporter.vue"
+import TheTaskCopier from "@/components/TheTaskCopier.vue"
+import { onMounted, ref, watch } from "vue";
+import { state, type Task } from "@/socket"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+const tasks = ref<Task[]>([])
+const sortBy = ref<string>('Create At')
+const sortDirection = ref<number>(-1) // -1: desc, 1: asc
+
+const sort = () => {
+  console.log('sort')
+  tasks.value = tasks.value.slice().sort((a, b) => {
+    if (sortBy.value == 'Name') {
+      return a.name > b.name ? sortDirection.value : -sortDirection.value
+    } else if (sortBy.value == 'Algorithm') {
+      return a.args.algorithm > b.args.algorithm ? sortDirection.value : -sortDirection.value
+    } else if (sortBy.value == 'Create At') {
+      return a.date > b.date ? sortDirection.value : -sortDirection.value
+    } else {
+      return 0
+    }
+  })
+}
+
+onMounted(() => {
+  tasks.value = state.tasks
+  sort()
+})
+
+watch (state, (newVal) => {
+  tasks.value = newVal.tasks
+  sort()
+})
+
+</script>
+
+<template>
+  <div class="ts-box u-top-spaced">
+    <table class="ts-table is-celled is-single-line">
+      <thead>
+      <tr class="has-cursor-pointer">
+        <th v-for="col in ['Name', 'Algorithm', 'Create At']" @click="sortBy = col; sortDirection *= -1; sort()">
+          {{ col }} <span class="ts-icon" :class="{ 'is-sort-down-icon': sortDirection == -1, 'is-sort-up-icon': sortDirection == 1 }" v-if="sortBy == col"></span>
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <template v-for="task in tasks" :key="task.name">
+        <tr class="has-cursor-pointer" :data-toggle="task.name+':has-hidden'" @dblclick="router.push({ name: 'task', params: { name: task.name } })">
+          <td>{{ task.name }}</td>
+          <td class="is-collapsed">{{ task.args.algorithm }}</td>
+          <td class="is-collapsed">{{ task.date }}</td>
+        </tr>
+        <tr class="has-hidden" :data-name="task.name">
+          <td class="is-secondary is-padded is-insetted" colspan="3">
+            <div class="ts-grid">
+              <div class="column">
+                algorithm: {{ task.args.algorithm }} {{ task.args.algorithm_args }}<br />
+                learn: {{ task.args.learn_args }}<br />
+                helper: {{ task.args.helper }}<br />
+              </div>
+              <div class="column is-fluid is-end-aligned" style="white-space: initial !important">
+                <router-link class="ts-text is-link" :to="{ name: 'task', params: { name: task.name }}">manage</router-link>
+                <TheTaskRemover :task-name="task.name" wrapper="link" />
+                <TheTaskExporter :taskName="task.name" wrapper="link" />
+                <TheTaskCopier :task-name="task.name" wrapper="link" />
+              </div>
+            </div>
+          </td>
+        </tr>
+      </template>
+      </tbody>
+    </table>
+  </div>
+</template>
--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,40 +1,16 @@
 <script setup lang="ts">
 import { state, type Task } from "@/socket"
 import { onMounted, ref, watch } from 'vue'
-import { useRouter } from 'vue-router'
-import TheTaskExporter from "@/components/TheTaskExporter.vue"
-import TheTaskCopier from "@/components/TheTaskCopier.vue"
-import TheTaskRemover from "@/components/TheTaskRemover.vue";
-
-const router = useRouter()
+import TheTasksList from "@/components/TheTasksList.vue"
 
 const tasks = ref<Task[]>([])
-const sortBy = ref<string>('Create At')
-const sortDirection = ref<number>(-1) // -1: desc, 1: asc
-
-const sort = () => {
-  console.log('sort')
-  tasks.value = tasks.value.slice().sort((a, b) => {
-    if (sortBy.value == 'Name') {
-      return a.name > b.name ? sortDirection.value : -sortDirection.value
-    } else if (sortBy.value == 'Algorithm') {
-      return a.args.algorithm > b.args.algorithm ? sortDirection.value : -sortDirection.value
-    } else if (sortBy.value == 'Create At') {
-      return a.date > b.date ? sortDirection.value : -sortDirection.value
-    } else {
-      return 0
-    }
-  })
-}
 
 onMounted(() => {
   tasks.value = state.tasks
-  sort()
 })
 
 watch (state, (newVal) => {
   tasks.value = newVal.tasks
-  sort()
 })
 </script>
 
@@ -83,41 +59,5 @@ watch (state, (newVal) => {
   <div class="ts-grid is-end-aligned">
     <router-link :to="{ name: 'task-create' }"><button class="ts-button">Create</button></router-link>
   </div>
-  <div class="ts-box u-top-spaced">
-    <table class="ts-table is-celled is-single-line">
-      <thead>
-      <tr class="has-cursor-pointer">
-        <th v-for="col in ['Name', 'Algorithm', 'Create At']" @click="sortBy = col; sortDirection *= -1; sort()">
-          {{ col }} <span class="ts-icon" :class="{ 'is-sort-down-icon': sortDirection == -1, 'is-sort-up-icon': sortDirection == 1 }" v-if="sortBy == col"></span>
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <template v-for="task in tasks" :key="task.name">
-        <tr class="has-cursor-pointer" :data-toggle="task.name+':has-hidden'" @dblclick="router.push({ name: 'task', params: { name: task.name } })">
-          <td>{{ task.name }}</td>
-          <td class="is-collapsed">{{ task.args.algorithm }}</td>
-          <td class="is-collapsed">{{ task.date }}</td>
-        </tr>
-        <tr class="has-hidden" :data-name="task.name">
-          <td class="is-secondary is-padded is-insetted" colspan="3">
-            <div class="ts-grid">
-              <div class="column">
-                algorithm: {{ task.args.algorithm }} {{ task.args.algorithm_args }}<br />
-                learn: {{ task.args.learn_args }}<br />
-                helper: {{ task.args.helper }}<br />
-              </div>
-              <div class="column is-fluid is-end-aligned" style="white-space: initial !important">
-                <router-link class="ts-text is-link" :to="{ name: 'task', params: { name: task.name }}">manage</router-link>
-                <TheTaskRemover :task-name="task.name" wrapper="link" />
-                <TheTaskExporter :taskName="task.name" wrapper="link" />
-                <TheTaskCopier :task-name="task.name" wrapper="link" />
-              </div>
-            </div>
-          </td>
-        </tr>
-      </template>
-      </tbody>
-    </table>
-  </div>
+  <TheTasksList />
 </template>
